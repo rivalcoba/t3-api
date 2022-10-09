ebd9dbb06e27f6b48cc36c205deb177a29b87050
rivalcoba <ivan.rivalcoba@gmail.com>
2022-10-08 23:24:26 -0500

    ✨ Adding Http status

--- server/api/v1/posts/post.controller.js
+++ server/api/v1/posts/post.controller.js
@@ -1,10 +1,11 @@
+import HttpStatus from 'http-status'
 import Post from './post.model'
 
 export async function createPost (req, res) {
   try {
     const post = await Post.createPost(req.body, req.user._id)
-    return res.status(201).json(post)
+    return res.status(HttpStatus.CREATED).json(post)
   } catch (error) {
-    return res.status(400).json(error)
+    return res.status(HttpStatus.BAD_REQUEST).json(error)
   }
 }
--- server/api/v1/users/user.controller.js
+++ server/api/v1/users/user.controller.js
@@ -1,22 +1,23 @@
+import HttpStatus from 'http-status'
 import User from './user.model'
 
 // Exportando Action Method de registro o signUp
 export async function signUp (req, res) {
   try {
     const user = await User.create(req.body)
-    return res.status(201).json(user)
+    return res.status(HttpStatus.CREATED).json(user)
   } catch (e) {
-    return res.status(500).json(e)
+    return res.status(HttpStatus.BAD_REQUEST).json(e)
   }
 }
 
 // Action Method para el login
 export function login (req, res, next) {
-  res.status(200).json(req.user.toJson())
+  res.status(HttpStatus.OK).json(req.user.toJson())
   // return next()
 }
 
 export async function getUser (req, res) {
   const user = await User.findOne({ message: 'Private Route', email: 'ivan.rivalcoba@gmail.com' })
-  res.status(200).json(user)
+  res.status(HttpStatus.OK).json(user)
 }
