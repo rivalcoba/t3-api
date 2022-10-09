c95b9fa7d591253218804172773ef3882e8e90e2
rivalcoba <ivan.rivalcoba@gmail.com>
2022-10-08 22:52:26 -0500

    ⛑ Fix Post Validation

--- server/api/v1/posts/post.validations.js
+++ server/api/v1/posts/post.validations.js
@@ -4,7 +4,7 @@ export default {
   createPost: {
     body: Joi.object({
       title: Joi.string().min(3).required(),
-      test: Joi.string().min(10).required()
+      text: Joi.string().min(10).required()
     })
   }
 }
