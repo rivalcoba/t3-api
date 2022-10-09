e49628decd545052ab88a7e1fa54e49028e800e6
rivalcoba <ivan.rivalcoba@gmail.com>
2022-10-09 01:57:49 -0500

    ✨ Delete Post

--- server/api/v1/posts/post.controller.js
+++ server/api/v1/posts/post.controller.js
@@ -52,3 +52,19 @@ export async function updatePost (req, res) {
     return res.status(HttpStatus.BAD_REQUEST).json(error)
   }
 }
+
+// [D] Borrar un Post
+export async function deletePost (req, res) {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    if (!post.user.equals(req.user._id)) {
+      return res.sendStatus(HttpStatus.UNAUTHORIZED)
+    }
+
+    await post.remove()
+    return res.sendStatus(HttpStatus.OK)
+  } catch (error) {
+    return res.status(HttpStatus.BAD_REQUEST).json(error)
+  }
+}
--- server/api/v1/posts/post.routes.js
+++ server/api/v1/posts/post.routes.js
@@ -11,15 +11,18 @@ import { validate } from 'express-validation'
 
 const router = new Router()
 
-// POST /api/v1/post
+// [C] POST /api/v1/post
 router.post('/', authJwt, validate(postValidation.createPost), postController.createPost)
 
-// GET /api/v1/post/<id>
+// [R] GET /api/v1/post/<id>
 router.get('/:id', postController.getPostById)
-// GET /api/v1/post/<id>
+// [R] GET /api/v1/post/<id>
 router.get('/', postController.getPostsList)
 
-// PATCH /api/v1/post/<id>
+// [U] PATCH /api/v1/post/<id>
 router.patch('/:id', authJwt, validate(postValidation.updatePost), postController.updatePost)
+
+// [D] DELETE /api/v1/post/<id>
+router.delete('/:id', authJwt, postController.deletePost)
 
 export default router
